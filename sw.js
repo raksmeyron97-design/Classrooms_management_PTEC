5507ada8d01a63efffb7c620422e1fcc4f6ac135
raksmeyron97-design <raksmeyron97@gmail.com>
2026-03-09 21:06:42 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'ptec-Classroom-Management-v2';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+// ពេល Install វាលោតកូដថ្មីចូលភ្លាមៗ
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// លុប Cache ចាស់ៗចោលពេលមាន Update ថ្មី
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// ទាញយកកូដពីអុីនធឺណិតមុន (Network First) បើអត់សេវាទើបប្រើ Cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
